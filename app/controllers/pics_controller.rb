512e36e328aa519ad70ecbfe5d52ad780c233c49
Amin <amin7x6@gmail.com>
2018-06-10 21:02:04 -0700

    Add form partial

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,5 +3,24 @@ class PicsController < ApplicationController
   def index
   end
 
-  
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Yesss! it was posted!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+
 end
